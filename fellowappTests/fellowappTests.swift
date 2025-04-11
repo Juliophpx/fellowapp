dd3455ca778673d54ce7d6c84e13ed205492756a
Hello Fellow <julio.gonzalez@hellofellow.com>
2025-04-11 11:53:26 -0400

    Initial Commit

---
+++ fellowappTests/fellowappTests.swift
@@ -0,0 +1,17 @@
+//
+//  fellowappTests.swift
+//  fellowappTests
+//
+//  Created by Julio Gonzalez on 4/11/25.
+//
+
+import Testing
+@testable import fellowapp
+
+struct fellowappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
